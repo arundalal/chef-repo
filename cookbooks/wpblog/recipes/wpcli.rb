9832a3b43ead6348de76015fd38168db08ea4ffa
arundalal <arundalal786@gmail.com>
2016-11-18 06:19:56 +0000

    softlink corrected in wpcli

--- cookbooks/wpblog/recipes/wpcli.rb
+++ cookbooks/wpblog/recipes/wpcli.rb
@@ -6,7 +6,7 @@ bash "wpcli setup" do
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     mv  wp-cli.phar  /usr/local/bin/
     chmod +x /usr/local/bin/wp-cli.phar    
-    ln -s /usr/local/bin/wp-cli.phar
+    ln -s /usr/local/bin/wp-cli.phar /usr/bin/wp
     EOH
     not_if { ::File.exists?('/usr/local/bin/wp-cli.phar') }
 end
